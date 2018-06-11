0e9f31fa04554d14e041e98116158633c20be711
Name <sssssss>
2018-06-11 15:14:32 +0300

    final commit

--- app/src/main/java/com/example/reham/moviesapp/ApiClient.java
+++ app/src/main/java/com/example/reham/moviesapp/ApiClient.java
@@ -14,21 +14,11 @@ public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-// set your desired log level
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-// add your other interceptors …
-
-// add logging as last interceptor
-        httpClient.addInterceptor(logging);  // <-- this is the important line!
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .client(httpClient.build())
                     .build();
         }
         return retrofit;
--- app/src/main/java/com/example/reham/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/reham/moviesapp/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.reham.moviesapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
